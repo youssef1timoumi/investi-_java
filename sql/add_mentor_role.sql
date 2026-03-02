05e2191f8ca662f59476fe523d6364d866cb8fde
Seif <SeifEddine.BenAbdallah@esprit.tn>
2026-03-02 05:38:08 +0100

    integration collaboration avec events + users

--- sql/add_mentor_role.sql
+++
@@ -1,6 +0,0 @@
--- Migration script to add 'mentor' role to users table
-USE 3a8;
-
--- Alter the role column to include 'mentor'
-ALTER TABLE users 
-MODIFY COLUMN role ENUM('admin', 'investor', 'innovator', 'mentor', 'user') NOT NULL;
